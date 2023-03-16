4b68da42790c8cdc62a7b8068deac5c4643cd801
cbganer@gmail.com <caiobarbosa2404@hotmail.com>
2023-03-16 13:42:29 -0300

    Performance de consultas

--- src/main/java/br/com/caio/loja/modelo/Pedido.java
+++ src/main/java/br/com/caio/loja/modelo/Pedido.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class Pedido {
 	@Column(name = "valor_total")
 	private BigDecimal valorTotal = BigDecimal.ZERO;
 	private LocalDate data = LocalDate.now();
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 	
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
@@ -37,7 +38,7 @@ public class Pedido {
 	
 	public void adicionarItem(ItemPedido item) {
 		item.setPedido(this);
-		this.itens.add(item);
+		this.getItens().add(item);
 		this.valorTotal = this.valorTotal.add(item.getValor());
 	}
 
@@ -75,6 +76,14 @@ public class Pedido {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedido> itens) {
+		this.itens = itens;
 	}
 	
 	
